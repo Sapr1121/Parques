ffaf33accd67e0603b30bbebcb078d92606c773b
Sapr1121 <sapr262004@gmail.com>
2025-11-25 11:11:43 -0500

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import { SocketProvider } from './contexts/SocketContext';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // StrictMode deshabilitado temporalmente para evitar doble ejecución de efectos
   // <React.StrictMode>
-    <SocketProvider>       
-      <App />
-    </SocketProvider>
+  <SocketProvider>
+    <App />
+  </SocketProvider>
   // </React.StrictMode>
 );
